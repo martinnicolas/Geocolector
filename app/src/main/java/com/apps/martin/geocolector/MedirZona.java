197623534114f2b48b0dc2c21ae5c5fe709ffefc
martinnicolas <ttincho77@gmail.com>
2017-02-23 18:10:40 -0300

    Agrego y cambio iconos en nav drawer

--- app/src/main/java/com/apps/martin/geocolector/MedirZona.java
+++ app/src/main/java/com/apps/martin/geocolector/MedirZona.java
@@ -1,6 +1,7 @@
 package com.apps.martin.geocolector;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
