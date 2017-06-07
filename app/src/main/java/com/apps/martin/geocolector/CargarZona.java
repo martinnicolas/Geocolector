4cb223018b531e0f732a80bae81017a8b1470d51
martinnicolas <ttincho77@gmail.com>
2017-06-07 19:08:54 -0300

    Agrego carga de mediciones pendientes de envio y strings

--- app/src/main/java/com/apps/martin/geocolector/CargarZona.java
+++ app/src/main/java/com/apps/martin/geocolector/CargarZona.java
@@ -1,12 +1,37 @@
 package com.apps.martin.geocolector;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import modelo.DaoSession;
+import modelo.RutaMedicion;
+import modelo.RutaMedicionDao;
+import utilidades.Session;
 
 
 /**
@@ -28,6 +53,12 @@ public class CargarZona extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private DaoSession daoSession;
+    private View rootView;
+
+    private View mProgressView;
+    private View mDCargarZonaForm;
 
     public CargarZona() {
         // Required empty public constructor
@@ -64,7 +95,170 @@ public class CargarZona extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cargar_zona, container, false);
+        daoSession = ((MainActivity)getActivity()).getDaoSession();
+        rootView = inflater.inflate(R.layout.fragment_cargar_zona, container, false);
+
+        final List<RutaMedicion> medidores = medidoresPendientes();
+        if (!medidores.isEmpty())
+            mostrarRuta();
+
+        mDCargarZonaForm = rootView.findViewById(R.id.cargar_zona_form);
+        mProgressView = rootView.findViewById(R.id.download_progress);
+        //Manejo el evento del boton cargar zona
+        Button btnCargarZona = (Button) rootView.findViewById(R.id.boton_cargar_zona);
+        btnCargarZona.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                try {
+                    enviarMedicionesPendientes(medidores);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+    /**
+     * Envía todas las mediciones pendientes al servidor
+     */
+    private void enviarMedicionesPendientes(List<RutaMedicion> medidores) throws JSONException {
+        for (RutaMedicion m:medidores) {
+            enviarMedicion(m);
+        }
+        mostrarRuta();
+    }
+
+    /**
+     * Envia los datos de la medición al servidor
+     * @param ruta
+     * @throws JSONException
+     */
+    public void enviarMedicion(final RutaMedicion ruta) throws JSONException {
+        showProgress(true);
+        SharedPreferences prefs = getActivity().getSharedPreferences("Configuracion", Context.MODE_PRIVATE);
+        String ip_server = prefs.getString("ip_server", "");
+        JSONObject credencials_and_data = new JSONObject();
+        credencials_and_data.put("nombre", Session.getSession().getUsuario());
+        credencials_and_data.put("user_token",Session.getSession().getUser_token());
+        credencials_and_data.put("medidor_id", ruta.getMedidor_id());
+        credencials_and_data.put("novedad_id",ruta.getNovedadId());
+        credencials_and_data.put("estado_actual",ruta.getEstado_actual());
+        credencials_and_data.put("estado_anterior",ruta.getEstado_anterior());
+        credencials_and_data.put("promedio",ruta.getPromedio());
+        credencials_and_data.put("demanda",ruta.getDemanda());
+        credencials_and_data.put("observacion",ruta.getObservacion());
+        credencials_and_data.put("fecha_medicion",ruta.getFecha());
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        JsonObjectRequest getRequest = new JsonObjectRequest("http://"+ip_server+"/restful/guardar_medicion",
+                credencials_and_data,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            procesarRespuesta(response,ruta);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        procesarRespuestaErronea();
+                    }
+                }
+        );
+        queue.add(getRequest);
+    }
+
+    /**
+     * Procesa la respuesta del servidor
+     *
+     * @param response
+     */
+    public void procesarRespuesta(JSONObject response, RutaMedicion ruta) throws JSONException {
+        if (response.has("errors")){
+            Toast.makeText(getActivity().getApplicationContext(), response.getString("errors"), Toast.LENGTH_SHORT).show();
+        }
+        else{
+            ruta.setAck(true);
+            ruta.update();
+        }
+        showProgress(false);
+    }
+
+    /**
+     * Procesa una respuseta erronea por parte del servidor
+     */
+    public void procesarRespuestaErronea(){
+        Toast.makeText(getActivity().getApplicationContext(), "No se pudo establecer la conexion \nVerifique la configuracion.", Toast.LENGTH_SHORT).show();
+        showProgress(false);
+    }
+
+    /**
+     * Obtiene una lsita de medidores pendientes de envio
+     * @return Lista de medidores pendientes de envio
+     */
+    private List<RutaMedicion> medidoresPendientes(){
+        //Tomo medidores cuyas mediciones no fueron recibidos por el servidor y los muestro
+        RutaMedicionDao rutaMedicionDao = daoSession.getRutaMedicionDao();
+        List<RutaMedicion> medidores = rutaMedicionDao.queryBuilder().where(
+                RutaMedicionDao.Properties.Ack.eq(false),
+                RutaMedicionDao.Properties.Medido.eq(true)).orderAsc().list();
+        return medidores;
+    }
+
+    /**
+     * Muestra los medidores cuyas mediciones no fueron recibidas por el servidor
+     */
+    private void mostrarRuta(){
+        ListView ruta = (ListView) rootView.findViewById(R.id.ruta);
+        List<RutaMedicion> medidores = medidoresPendientes();
+        ArrayAdapter<RutaMedicion> adapter = new ArrayAdapter<RutaMedicion>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                medidores
+        );
+        ruta.setAdapter(adapter);
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mDCargarZonaForm.setVisibility(show ? View.GONE : View.VISIBLE);
+            mDCargarZonaForm.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mDCargarZonaForm.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mDCargarZonaForm.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
